08f8b94599796e3332ab00c9d16e6b837a96d01b
tasha kijsamnong <tkijsamnong@yahoo.com>
2014-08-27 01:12:57 -0400

    todolist started

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,3 +15,12 @@ var UserCollection = Parse.Collection.extend({
 /////////////////////
 ///////todolist/////
 ///////////////////
+$(document).ready(function() {
+    $(".button").click(function() {
+        var toAdd = $("input[name=checkListItem]").val();
+        $(".list").append('<div class="item">' + toAdd + "</div>");
+        $(document).on('click', '.item', function(){
+        $(this).remove();
+    });
+    });
+});
